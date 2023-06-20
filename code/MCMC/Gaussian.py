cae678b119478eff2b88d9ccb8d80372b5189d48
pmorande27 <pmorande27@gmail.com>
2023-06-20 13:03:34 +0200

    Add Jackknife and other dists

--- code/MCMC/Gaussian.py
+++ code/MCMC/Gaussian.py
@@ -7,9 +7,13 @@ from scipy import rand
 import matplotlib.animation as animation
 
 def s(x):
-    return x**2/2
-def metropolis(x):
-    deltaX = random.uniform(-0.5,0.5)
+    return -np.log(np.exp(-x**2/2)+ np.exp(-(x-20)**2/2))
+def metropolis(x,n):
+    if n %2 ==0:
+        deltaX = random.uniform(-0.5,0.5)
+    else: 
+        deltaX = random.uniform(-100,100)
+
     xprime = x + deltaX
     deltaS = s(xprime)-s(x)
     r = random.random()
@@ -21,15 +25,17 @@ def run(n):
     #for j in range(100):
     x0= 0 
     for i in range(n):
-        x0 = metropolis(x0)
+        x0 = metropolis(x0,i)
         outputs[i] = x0 
     return outputs
     """plt.hist(outputs, bins = int(18000/15)
                 )
     plt.show()"""
 def plot(n):
-    plt.hist(run(n), bins = int(18000/15)
-                )
+    sns.distplot(run(n), hist=False, kde=True, 
+             bins=int(180/5), color = 'darkblue', 
+             hist_kws={'edgecolor':'black'},
+             kde_kws={'linewidth': 4})
     plt.show()
 def average(n):
     output = run(n)
@@ -51,4 +57,22 @@ def run_obsevable(n,f):
     plt.xlim(1,n)
     plt.show()
 
-run_obsevable(10**6, lambda x: x**3)
+def error(n,f,number_of_bins):
+    outputs = run(n)
+    slices = np.split(np.array(outputs),number_of_bins)
+    w = len(slices[0])
+    aux = sum([ calc_f_w_k(slices,f,number_of_bins,w,k) for k in range(number_of_bins)])
+    average_f = aux/number_of_bins
+    b = [(sum([f(x) for x in slices[k]])/w - average_f)**2 for k in range(number_of_bins)]
+    aux_3 =sum(b)
+    aux_3 = np.sqrt(1/(number_of_bins*(number_of_bins-1))*aux_3)
+    return aux_3
+def calc_f_w_k(slices,f,number_of_bins,w,k):
+    aux = 0
+    for i in range(number_of_bins):
+        if i != k:
+            aux += sum([f(x) for x in slices[i]])
+    return aux/((number_of_bins-1)*w)
+
+#print(error( 50000, lambda x: x**2, 2500))
+plot(10**5)
