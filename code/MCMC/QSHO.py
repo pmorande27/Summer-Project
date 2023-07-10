b46d6f5f7151b1497b39b11659d4c58ebd3705cc
Pmorande27 <pmorande27@gmail.com>
2023-07-10 16:25:22 +0100

    Add Object oriented design

--- code/MCMC/QSHO.py
+++ code/MCMC/QSHO.py
@@ -3,45 +3,59 @@ import random
 import matplotlib.pyplot as plt
 def analytic(t,N,e):
     return 1/2*( np.exp(-t)+np.exp(t)*np.exp(-N*e))
-def main():
-    # Parameters
-    N = 16
-    N_0 = N
-    e = 0.3125
-    Nmc = 10**5
-    Nsweeps =3
-    positions = [i for i in range(N)]
-    Delta = 1
-    total_time = N_0*e
-    values = [[] for i in range(len(positions))]
-    sites = np.zeros(N)
-    means = []
-    counter =0
-    for i in range(Nmc):
-        print(i)
-        for j in range(N):
+
+
+class Lattice(object):
+    def __init__(self, N, a, N_thermal, N_mc, N_correlation, Delta) -> None:
+        self.N_thermal = N_thermal
+        self.N_correlation = N_correlation
+        self.N_mc = N_mc
+        self.a = a
+        self.N = N
+        self.Delta = Delta
+        self.lattice = np.zeros(N)
+        self.total_time = N*a
+        self.sweeps = 0
+        self.thermalize()
+    def sweep(self):
+        for j in range(self.N):
             pos =j
-            delta = random.uniform(-Delta,Delta)
-            candidate = sites[pos] + delta
-            next_pos = (pos+1)%(N)
-            pre_pos = (pos-1)%(N)
-            dS = 1/2* ((sites[next_pos]-candidate)**2/e) + 1/2 *((candidate-sites[pre_pos])**2/e) -1/2 *((sites[next_pos]-sites[pos])**2/e) -1/2* ((sites[pos]-sites[pre_pos])**2/e) + e*1/8 *(sites[next_pos]+candidate)**2 +e*1/8 *(candidate+sites[pre_pos])**2-e*1/8 *(sites[next_pos]+sites[pos])**2-e*1/8 *(sites[pre_pos]+sites[pos])**2
+            delta = random.uniform(-self.Delta,self.Delta)
+            candidate = self.lattice[pos] + delta
+            next_pos = (pos+1)%(self.N)
+            pre_pos = (pos-1)%(self.N)
+            dS = 1/2* ((self.lattice[next_pos]-candidate)**2/self.a) + 1/2 *((candidate-self.lattice[pre_pos])**2/self.a) -1/2 *((self.lattice[next_pos]-self.lattice[pos])**2/self.a) -1/2* ((self.lattice[pos]-self.lattice[pre_pos])**2/self.a) + self.a*1/8 *(self.lattice[next_pos]+candidate)**2 +self.a*1/8 *(candidate+self.lattice[pre_pos])**2-self.a*1/8 *(self.lattice[next_pos]+self.lattice[pos])**2-self.a*1/8 *(self.lattice[pre_pos]+self.lattice[pos])**2
             probs = random.random()
             if probs < min(1,np.exp(-dS)):
-                sites[pos] = candidate
-        if counter == Nsweeps:
-            for i in range(len(positions)):
-                values[i] += [sites[0]*sites[positions[i]]]
-            counter =0
-        counter+=1
-    means = [np.mean(value) for value in values] + [np.mean(values[0])]
-    times = [e*position for position in positions] + [e*N]
-    print(times)
-    ts = np.linspace(start=0,stop=total_time)
-    corr = [analytic(t,N,e) for t in ts]
+                self.lattice[pos] = candidate
+    def thermalize(self):
+        for i in range(self.N_thermal):
+            self.sweep()
+    def measure_twopoint(self):
+        positions = [i for i in range(self.N)]
+        values = [[] for i in range(len(positions))]
+        for i in range(self.N_mc):
+            print(i)
+            self.sweep()
+            if self.sweeps % self.N_correlation ==0:
+                for i in range(len(positions)):
+                    values[i] += [self.lattice[0]*self.lattice[positions[i]]]
+            self.sweeps+=1
+        means = [np.mean(value) for value in values] + [np.mean(values[0])]
+        times = [self.a*position for position in positions] + [self.a*self.N]
+        return means,times
+
+
+
+def main():
+    N = 16
+    a = 0.3125
+    lat = Lattice(N=N,a=a,N_thermal=100,N_mc=10**5,N_correlation=3,Delta=1)
+    means,times = lat.measure_twopoint()
+    
+    ts = np.linspace(start=0,stop=N*a)
+    corr = [analytic(t,N,a) for t in ts]
     plt.plot(times,means,'o')
     plt.plot(ts,corr)
     plt.show()
 main()
-
-# 
