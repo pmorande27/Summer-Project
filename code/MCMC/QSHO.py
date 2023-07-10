f929cc3079b7c715db4a54285d5a5639b8e4dbb4
Pmorande27 <pmorande27@gmail.com>
2023-07-10 17:29:04 +0100

    Lint

--- code/MCMC/QSHO.py
+++ code/MCMC/QSHO.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random 
 import matplotlib.pyplot as plt
-def analytic(t,N,e):
+def analytic(t, N, e):
     return 1/2*( np.exp(-t)+np.exp(t)*np.exp(-N*e))
 
 
@@ -24,26 +24,37 @@ class Lattice(object):
             candidate = self.lattice[pos] + delta
             next_pos = (pos+1)%(self.N)
             pre_pos = (pos-1)%(self.N)
+
+            #Entropy change
             dS = (1/(2*self.a)*( (self.lattice[next_pos]-candidate)**2 + (candidate-self.lattice[pre_pos])**2 -(self.lattice[next_pos]-self.lattice[pos])**2 -(self.lattice[pos]-self.lattice[pre_pos])**2 )+ 
             self.a*1/8* ( (self.lattice[next_pos]+candidate)**2 + (candidate+self.lattice[pre_pos])**2-(self.lattice[next_pos]+self.lattice[pos])**2- (self.lattice[pre_pos]+self.lattice[pos])**2))
+           
+           #Metropolis step
             probs = random.random()
-            if probs < min(1,np.exp(-dS)):
+            if probs < min(1, np.exp(-dS)):
                 self.lattice[pos] = candidate
+
     def thermalize(self):
+
         for i in range(self.N_thermal):
             self.sweep()
+
     def measure_twopoint(self):
+
         positions = [i for i in range(self.N)]
         values = [[] for i in range(len(positions))]
+
         for i in range(self.N_mc):
             print(i)
             self.sweep()
-            if self.sweeps % self.N_correlation ==0:
+            if self.sweeps % self.N_correlation == 0:
                 for i in range(len(positions)):
                     values[i] += [self.lattice[0]*self.lattice[positions[i]]]
             self.sweeps+=1
+
         means = [np.mean(value) for value in values] + [np.mean(values[0])]
         times = [self.a*position for position in positions] + [self.a*self.N]
+
         return means,times
 
 
@@ -55,13 +66,13 @@ def main():
     N_correlation = 3
     N_thermal = 10**3
     Delta = 1
-    lat = Lattice(N=N,a=a,N_thermal=N_thermal,N_mc=N_mc,N_correlation=N_correlation,Delta=Delta)
+    lat = Lattice(N = N, a = a, N_thermal = N_thermal, N_mc = N_mc, N_correlation = N_correlation, Delta = Delta)
     means,times = lat.measure_twopoint()
     
-    ts = np.linspace(start=0,stop=N*a)
-    corr = [analytic(t,N,a) for t in ts]
-    plt.plot(times,means,'o')
-    plt.plot(ts,corr)
+    ts = np.linspace(start = 0, stop = N*a)
+    corr = [analytic(t, N, a) for t in ts]
+    plt.plot(times, means, 'o')
+    plt.plot(ts, corr)
     plt.show()
 main()
 
