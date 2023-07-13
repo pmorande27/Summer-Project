f086941bc461ce4f2c5ffa2440790ca9e41a6dd5
Pmorande27 <pmorande27@gmail.com>
2023-07-13 15:40:50 +0100

    Add proper correlator code

--- code/MCMC/QSHO.py
+++ code/MCMC/QSHO.py
@@ -16,6 +16,7 @@ class Lattice(object):
         self.lattice = np.zeros(N)
         self.total_time = N*a
         self.sweeps = 0
+        self.accepted = 0
         self.thermalize()
     def sweep(self):
         for j in range(self.N):
@@ -33,11 +34,13 @@ class Lattice(object):
             probs = random.random()
             if probs < min(1, np.exp(-dS)):
                 self.lattice[pos] = candidate
+                #self.accepted += 1
 
     def thermalize(self):
 
         for i in range(self.N_thermal):
             self.sweep()
+        self.accepted = 0
 
     def measure_twopoint(self):
 
@@ -45,7 +48,6 @@ class Lattice(object):
         values = [[] for i in range(len(positions))]
 
         for i in range(self.N_mc):
-            print(i)
             self.sweep()
             if self.sweeps % self.N_correlation == 0:
                 for i in range(len(positions)):
@@ -56,23 +58,61 @@ class Lattice(object):
         times = [self.a*position for position in positions] + [self.a*self.N]
 
         return means,times
+    
+    def measure_greens(self):
+
+        positions = [i for i in range(self.N)]
+        values = [[] for n in range(self.N)]
+
+        for i in range(self.N_mc):
+            self.sweep()
+            print(i)
+            if self.sweeps % self.N_correlation == 0:
+                for n in range(self.N):
+                    values[n] += [1/self.N* sum([self.lattice[((j+n)%self.N)]* self.lattice[j] for j in range(self.N)])]
+            self.sweeps+=1
+
+        means = [np.mean(value) for value in values] + [np.mean(values[0])]
+        times = [self.a*n for n in range(self.N)] + [self.a*self.N]
+
+        return means,times
+
+    def measure_acceptance(self):
+        values = np.zeros(self.N_mc)
+        for i in range(self.N_mc):
+            self.sweep()
+            self.sweeps+=1
+            values[i] = float(self.accepted)/self.N
+            self.accepted = 0
+        return values,[i for i in range(1,self.N_mc+1)]
 
 
 
 def main():
-    N = 16
-    a = 0.3125
+    N = 100
+    a = 0.05
     N_mc = 10**6
-    N_correlation = 3
-    N_thermal = 10**3
-    Delta = 1
+    N_correlation = 20
+    N_thermal = 200
+    Delta = 1.4
     lat = Lattice(N = N, a = a, N_thermal = N_thermal, N_mc = N_mc, N_correlation = N_correlation, Delta = Delta)
-    means,times = lat.measure_twopoint()
+
+    means,times = lat.measure_greens()
     
     ts = np.linspace(start = 0, stop = N*a)
     corr = [analytic(t, N, a) for t in ts]
-    plt.plot(times, means, 'o')
-    plt.plot(ts, corr)
+    #plt.plot(times, means, 'o')
+    #plt.plot(ts, corr)
+    DE = [abs(np.log(means[i]/means[i+1])/a) for i in range(0,N-1)]
+    plt.plot([j for j in range(len(DE))],DE,'o')
+    plt.ylim(0,2)
     plt.show()
+
+    #values, times = lat.measure_acceptance()
+    #print(np.mean(values))
+    #plt.scatter(times, values)
+    #plt.show()
+
+    
 main()
 
