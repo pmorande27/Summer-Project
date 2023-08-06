90ae24929c893269cd93305f50e40b00678b251a
Pmorande27 <pmorande27@gmail.com>
2023-08-06 14:04:11 +0100

    Lint

--- code/MCMC/Gaussian.py
+++ code/MCMC/Gaussian.py
@@ -5,61 +5,111 @@ import seaborn as sns
 import numpy as np
 from scipy import rand
 import matplotlib.animation as animation
+
 class Gaussian(object):
     def __init__(self,epsilon, N_thermal, N_measurment, N_sweeps) -> None:
+
         self.lattice = 0
+
         self.N_thermal = N_thermal
+        
         self.N_measurement = N_measurment
+        
         self.N_sweeps = N_sweeps
+        
         self.epsilon = epsilon
+        
         self.thermalize()
+    
     @staticmethod
     def s(x):
+    
         return x**2/2
+    
     def sweep(self):
+    
         deltaX = np.random.uniform(-self.epsilon,self.epsilon)
+    
         xprime = self.lattice + deltaX
+    
         Delta_S = Gaussian.s(xprime)-Gaussian.s(self.lattice)
+    
         r = random.random()
+    
         if Delta_S <0 or np.exp(-Delta_S) > np.random.uniform(0,1):
+    
             self.lattice = xprime
 
     def thermalize(self):
+    
         for i in range(self.N_thermal):
+    
             self.sweep()
+    
     def measure(self, observable):
+    
         results = [0 for i in range(self.N_measurement)]
+    
         for i in range(self.N_measurement):
+    
             for j in range(self.N_sweeps):
+    
                 self.sweep()
+    
             print(i)
+    
             results[i] = observable(self.lattice)
+    
         return(results)
     
     def autocorrelation(self, observable):
+    
         n = 45
+    
         steps = [i for i in range(n)]
+    
         measurements = [0 for i in range(self.N_measurement)]
+    
         for i in range(self.N_measurement):
+    
             print(i)
+    
             self.sweep()
+    
             measurements[i] = observable(self.lattice)
+    
         average = np.average(measurements)
+    
         average_of_sq = np.average([m**2 for m in measurements])
+    
         sigma_sq = (average_of_sq-average**2)
+    
         results = [0 for i in range(n)]
+    
         for step in steps:
+    
             result = 0
+    
             for i in range(self.N_measurement-step):
+    
                 result += (measurements[i]-average)*(measurements[i+step]-average)
+    
             result = (result/(self.N_measurement-step))/ sigma_sq
+    
             results[step] = result 
+    
         plt.plot(results,'o')
+    
         plt.show()
+    
         return results
+
 def main():
+
     Gauss = Gaussian(epsilon=3,N_thermal=100,N_measurment=100000,N_sweeps=10)
+
     print(Gauss.autocorrelation(lambda x: x))
+
 main()
 
 
